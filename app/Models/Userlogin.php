ceb27b296fe7c2281084c217b08475fd5d2a0072
enzo-l-manfio <enzolargermanfio@gmail.com>
2026-01-21 17:18:47 -0300

    Nomes de classes e apagando controller e models de Eventos (Achei que era atribuição do nosso grupo :|)

--- app/Models/Userlogin.php
+++ app/Models/Userlogin.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class userlogin extends Authenticatable
+class Userlogin extends Authenticatable
 {
     protected $primaryKey = 'uid';
     public $timestamps = false;
