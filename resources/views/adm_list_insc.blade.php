c45bb0ef55caf27e0be9fd7cb341ac1349d1916b
Gabriel Paulino <118023793+gabriel-paulino2708@users.noreply.github.com>
2026-01-21 21:36:52 -0300

    Layouts aplicados! FÉRIAS!!!!

--- resources/views/adm_list_insc.blade.php
+++ resources/views/adm_list_insc.blade.php
@@ -1,42 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Lista de Participantes - SEMATRON</title>
-        <link rel="stylesheet" href="{{asset('/reset.css')}}">
-        <link rel="stylesheet" href="{{asset('/sematron.css')}}">
-        <!--Aqui em baixo importa a fonte. POR QUE CARALHOS INTER???????????? AQUI É GRÊMIO PORRA!!!!!!-->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    </head>
-<body>
+@extends('layouts.layout-basico')           <!--IMPORTANDO LAYOUT DA PASTA LAYOUT-->
 
-    <header>
-        <!--É literal só a listra laranja-->
-        <div class="listra-laranja"></div>
+@section('title', 'Lista de Participantes - SEMATRON')        <!--AQUI TU BOTA O NEGOCIO QUE APARECE NA ABA LÁ EM CIMA-->
 
-        <!--Aqui estão os links, na versão desktop-->
+@section('content')                         <!--AQUI COMEÇA O CONTEÚDO ESPECÍFICO DA PÁGINA-->
 
-        <div class="Parte-De-Cima">
-
-            <div class="Centraliza"><img class="Logo-Inicio" src="{{asset('/Imagens/logo-Photoroom.png')}}" alt="Logo da Sematron"></div>
-            <div class="Centraliza"><h1 class="Sematron-Inicio">SEMATRON XXII</h1></div>
-            
-            <div class="Links">
-                <a class="Link-Do-Topo" href="/inicio">Página Inicial</a>
-                <a class="Link-Do-Topo" href="/inscricoes">Inscrições</a>
-                <a class="Link-Do-Topo" href="/minicursos">Minicursos</a>
-                <a class="Link-Do-Topo" href="/visitas">Visitas</a>
-                <a class="Link-Do-Topo" href="/login">Login</a>
-                <a class="Link-Do-Topo" href="/cadastro">Cadastro</a>
-                <a class="Link-Do-Topo" href="/maisSematron">Mais Sematron</a>
-                <a class="Link-Do-Topo Direita" href="/contato">Contato</a>
-            </div>
-        </div>
-    </header>
-    
     <main class="main-container">
         
         <div class="page-header">
@@ -76,24 +43,8 @@
         <a href="/" class="btn-back">VOLTAR PARA HOME</a>
 
     </main>
+    
+@endsection                                 <!--AQUI ACABA O CONTEÚDO ESPECÍFICO DA PÁGINA-->
 
-    <div class="listra-laranja espacamento-rodape"></div>
-
-    <footer class="Rodape">
-
-        <h1 class="Creditos-Rodape">Créditos</h1>
-        <h1 class="Copyright-Rodape">Copyright © 2014–2025 Grupo SEMATRON. Todos os direitos reservados.</h1>
-        <h1 class="Av-Trabalhador">Av. Trabalhador São-Carlense, 400 • EESC/USP • São Carlos — SP</h1>
-        <h1 class="Telefone">Tel: +55 (61) 98172-5281 • Email: sematron@eesc.usp.br</h1>
-
-        <div class="Redes-Sociais-Rodape">
-            <a class="Borda-Rodape" href="https://www.instagram.com/sematronusp/">Instagram</a>
-            <a class="Borda-Rodape" href="https://www.youtube.com/@sematronusp">YouTube</a>
-            <a class="Borda-Rodape" href="/inicio">Site</a>
-            <!--Por favor não apague esse EasterEgg foi feito com muito carinho por mim ;)-->
-            <a class="Borda-Rodape-Gigante" href="https://www.ubirata.pr.gov.br/">Ubiratã</a>
-        </div>
-    </footer>
-
-    </body>
-</html>
+    
+    
---
+++ resources/views/layouts/parciais/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <!--É literal só a listra laranja-->
+    <div class="listra-laranja"></div>
+
+    <!--Aqui estão os links, na versão desktop-->
+
+    <div class="Parte-De-Cima">
+
+        <div class="Centraliza"><img class="Logo-Inicio" src="{{asset('/Imagens/logo-Photoroom.png')}}" alt="Logo da Sematron"></div>
+        <div class="Centraliza"><h1 class="Sematron-Inicio">SEMATRON XXII</h1></div>
+                
+        <div class="Links">
+            <a class="Link-Do-Topo" href="/inicio">Página Inicial</a>
+            <a class="Link-Do-Topo" href="/inscricoes">Inscrições</a>
+            <a class="Link-Do-Topo" href="/minicursos">Minicursos</a>
+            <a class="Link-Do-Topo" href="/visitas">Visitas</a>
+            <a class="Link-Do-Topo" href="/login">Login</a>
+            <a class="Link-Do-Topo" href="/cadastro">Cadastro</a>
+            <a class="Link-Do-Topo" href="/maisSematron">Mais Sematron</a>
+            <a class="Link-Do-Topo Direita" href="/contato">Contato</a>
+        </div>
+    </div>
+</header>
