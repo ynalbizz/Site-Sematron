cadf6e2dbeab23e098acd4a33c82254022c464bf
Gabriel Paulino <118023793+gabriel-paulino2708@users.noreply.github.com>
2026-01-26 16:55:33 -0300

    Inscricoes arrumado

---
+++ resources/views/layouts/parciais/head.blade.php
@@ -0,0 +1,17 @@
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title', 'Minha Aplicação')</title>
+        <link rel="stylesheet" href="{{asset('/sematron.css')}}">
+        <link rel="stylesheet" href="{{asset('/reset.css')}}">
+        <!--Importando o Java-->
+        <script src="{{asset('javaDaSematron/ver-senha.js') }}" defer></script>
+        <script src="{{asset('javaDaSematron/inscricao.js') }}" defer></script>
+        <script src="{{asset('javaDaSematron/perfil.js') }}" defer></script>
+        <!--Isso é onde tem o olhinho da senha-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <!--Aqui em baixo importa a fonte. POR QUE CARALHOS INTER???????????? AQUI É GRÊMIO PORRA!!!!!!-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    </head>
---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.layout-logado')           <!--IMPORTANDO LAYOUT DA PASTA LAYOUT-->
+
+@section('title', 'Perfil')                 <!--AQUI TU BOTA O NEGOCIO QUE APARECE NA ABA LÁ EM CIMA-->
+
+@section('content')                         <!--AQUI COMEÇA O CONTEÚDO ESPECÍFICO DA PÁGINA-->
+    <section class="separa-em-tres">
+        <div class="Parte-da-Esquerda">
+            <h1 class="Login-grande">Perfil</h1>
+            <h1 class="Sub-Login">Veja aqui suas informações.</h1>
+        </div>
+
+
+
+        <div class="borda-perfil">
+            <div class="texto-perfil">
+                <h1 class="nome-bendito">Enzo Marina Lombada</h1>
+                <h1 class="email-do-ser">amigosdogil10@gmail.com</h1>
+                <h1 class="curso-que-faz">EESC-USP | Eng. Mecatrônica</h1>
+                <h1 class="situacao-do-pix">Pagamento Confirmado!</h1>
+                <a href="" class="infos-pessoas">Informações Pessoais</a>
+            </div>
+        </div>
+
+
+
+        <div class="borda-perfil">
+            <div class="texto-perfil">
+                <h1 class="segue-o-lider">Acompanhe seus dados</h1>
+                <h1 class="palavras-bonitas">Selecione qual das Sematrons que você deseja ver seus dados:</h1>
+                <div class="perfil-bagulho">
+                    <div class="input-group">
+                        <select id="semattt" name="pacote-dos-guri" required>
+                            <option value="" disabled selected>Selecione...</option>
+                            <option value="Sematron-XXII">SEMATRON XXII</option>
+                            <option value="Sematron-XXI">SEMATRON XXI</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="vaiSumir" data-show="Sematron-XXII">
+        <div class="Parte-da-Esquerda">
+            <h1 class="Login-grande">SEMATRON XXII</h1>
+        </div>
+
+
+
+        <div class="textos-perfil">
+            <div class="div-Perfil">
+                <h1 class="Tit-Perfil">Categoria:</h1>
+                <h2 class="SubTit-Perfil">Participante</h2>
+            </div>
+
+            <div class="div-Perfil">
+                <h1 class="Tit-Perfil">Kit:</h1>
+                <h2 class="SubTit-Perfil">Completo</h2>
+            </div>
+
+            <div class="div-Perfil">
+                <h1 class="Tit-Perfil">Minicursos:</h1>
+                <h2 class="SubTit-Perfil">Jogar Truco</h2>
+            </div>
+
+            <div class="div-Perfil">
+                <h1 class="Tit-Perfil">Visita:</h1>
+                <h2 class="SubTit-Perfil">Ubiratã</h2>
+            </div>
+
+            <div class="div-Perfil">
+                <h1 class="Tit-Perfil">Camiseta:</h1>
+                <h2 class="SubTit-Perfil">G</h2>
+            </div>
+        </div>
+
+
+
+        <div class="textos-perfil">
+            <div class="div-Perfil">
+                <h1 class="Tit-Perfil">Presença em Palestras:</h1>
+                <h2 class="SubTit-Perfil">X%</h2>
+            </div>
+
+            <div class="borda-certificado">
+                <div class="certificados">
+                    <h1 class="palavra-certificado">Certificados</h1>
+                </div>
+
+                <div class="espacamento-boto">
+                    <a href="" class="botoes-perfil">Presença</a>
+                    <a href="" class="botoes-perfil">Minicurso</a>
+                    <a href="" class="botoes-perfil">Visita</a>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection                                 <!--AQUI ACABA O CONTEÚDO ESPECÍFICO DA PÁGINA-->
