dbb2e2181925d5252cb30f54ec94e4393e355a6e
Gabriel Paulino <118023793+gabriel-paulino2708@users.noreply.github.com>
2026-01-29 13:50:21 -0300

    Corriji, retorna array!

--- resources/views/inscricoes.blade.php
+++ resources/views/inscricoes.blade.php
@@ -50,7 +50,7 @@
             <h1 class="Champions-do-Forms">Escolha a Visita</h1>
                 <div class="input-group">
                     <label>Visita</label>
-                        <select name="visita[]" multiple>
+                        <select name="visita[]">
                             <option value="" disabled selected>Selecione...</option>
                             <option value=0>Básico R$</option>
                             <option value=1>Completo R$</option>
@@ -65,7 +65,7 @@
             <h1 class="Champions-do-Forms">Escolha o Minicurso</h1>
                 <div class="input-group">
                     <label>Minicurso</label>
-                        <select name="minicurso[]" multiple>
+                        <select name="minicurso[]">
                             <option value="" disabled selected>Selecione...</option>
                             <option value=0>Básico R$</option>
                             <option value=1>Completo R$</option>
