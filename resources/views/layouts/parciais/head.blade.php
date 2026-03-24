bc3c7b4417f1c7e893e23c86524af43ea40e0b98
ynalbizz <cdavic13@gmail.com>
2026-03-24 11:26:25 -0300

    fix: remove versioning from sematron.css link and add perfil.css styles

--- resources/views/layouts/parciais/head.blade.php
+++ resources/views/layouts/parciais/head.blade.php
@@ -2,7 +2,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title', 'Minha Aplicação')</title>
-        <link rel="stylesheet" href="{{asset('/sematron.css?v=2.0)}}">
+        <link rel="stylesheet" href="{{asset('/sematron.css')}}">
         <link rel="stylesheet" href="{{asset('/reset.css')}}">
         <!--Importando o Java-->
         <script src="{{asset('javaDaSematron/ver-senha.js') }}" defer></script>
