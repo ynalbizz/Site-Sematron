cb888a81676681cb08ed2770d2662f78c9daee2c
ynalbizz <cdavic13@gmail.com>
2026-03-24 11:04:55 -0300

    fix:  typo erro

--- resources/views/components/⚡perfil.blade.php
+++ resources/views/components/⚡perfil.blade.php
@@ -38,7 +38,7 @@ new #[Layout('layouts.layout-logado')] class extends Component
             if($user->temInscricaoCompleta()){
                 return 'confirmed';
             }
-            $user_pids = Userdata::where('uid',$user->uid)->where('sid',config('general.sematon_atual'))->pluck('pid');
+            $user_pids = Userdata::where('uid',$user->uid)->where('sid',config('general.sematron_atual'))->pluck('pid');
             $first_sale = Sale::whereIn('pid',$user_pids)->first();
             if($first_sale==null){return 'failed';}
             return $first_sale->status;
