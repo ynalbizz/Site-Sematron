423beeb088b2fbb28a0d4551a17df86e8eb2c63c
ynalbizz <cdavic13@gmail.com>
2026-04-08 23:01:33 -0300

    fix: update main styles for Inscricao Fechada view to improve layout and structure

--- resources/views/livewire/insc-fechada.blade.php
+++ resources/views/livewire/insc-fechada.blade.php
@@ -1,17 +1,17 @@
-
-<main style="    flex: 1; 
-    display: flex;
-    justify-content: center; 
-    align-items: center;     
-    text-align: center;">
-    <div class="texto-fechado" 
-         wire:key="lw-2595669341-0" 
-         wire:id="w4SrrTiYvOeN20uYByyB"
-         style="font-family: 'Inter', sans-serif;
-    font-size: 2rem;
-    font-weight: bold;
-    color: #797979;
-    text-transform: uppercase;">
+<main 
+    wire:key="lw-2595669341-0" 
+    wire:id="tMRmGwsmGCYiXESBeB9b" 
+    style="
+        display: flex;
+        justify-content: center; 
+        align-items: center;      
+        text-align: center;
+        min-height: 100vh; /* Faz o elemento ter no mínimo a altura da tela */
+        width: 100%;       /* Garante que ocupe toda a largura */
+        margin: 0;         /* Remove margens padrão */
+    ">
+    
+    <div class="texto-fechado" style="font-family: 'Inter', sans-serif; font-size: 2rem; font-weight: bold; color: #797979; text-transform: uppercase;">
         Inscrições Fechadas
     </div>
 </main>
