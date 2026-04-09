141af9a5a08f3ace06306a38d8abfefa862b51d1
ynalbizz <cdavic13@gmail.com>
2026-04-09 12:01:26 -0300

    caceta

--- resources/views/palestras.blade.php
+++ resources/views/palestras.blade.php
@@ -16,13 +16,18 @@
         <section class="espaco-no-topo margem-esquerda">
             <div class="imagem-de-fundo">
                 @foreach ($palestras as $palestra)
-                <div class="borda-visitas">
-                    <div class="texto-na-esquerda">
-                        <h1 class="nome-da-visita">Palestra — {{ $palestra->name }}</h1>
-                        <h1 class="horarios-visitas">Dia: {{date('d/m', strtotime($palestra->start))}} • Hora: {{date('H:i', strtotime($palestra->start))}} </h1>
+                @if ($palestra->slots >= 1)
+                    <div class="borda-visitas">
+                        <div class="texto-na-esquerda">
+                            <h1 class="nome-da-visita">Palestra — {{ $palestra->name }}</h1>
+                            <h1 class="horarios-visitas">Dia: {{date('d/m', strtotime($palestra->start))}} • Hora: {{date('H:i', strtotime($palestra->start))}} </h1>
+                            <h1 class="horarios-visitas">Vagas: {{ $palestra->slots }}</h1>
+
+                        </div>
+                        <a href="/inscricao" class="botao-inscrever">Inscrever</a>
                     </div>
-                    <a href="/inscricao" class="botao-inscrever">Inscrever</a>
-                </div>
+                @else
+                @continue
                 @endforeach
 
 
