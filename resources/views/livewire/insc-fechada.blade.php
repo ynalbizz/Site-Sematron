062a6da05f3fb6c0068b95185895e559a8f0572b
ynalbizz <cdavic13@gmail.com>
2026-04-08 22:50:03 -0300

    fix: simplify main and texto-fechado styles for cleaner structure in Inscricao Fechada view

--- resources/views/livewire/insc-fechada.blade.php
+++ resources/views/livewire/insc-fechada.blade.php
@@ -1,25 +1,17 @@
-<style>
-main {
-    flex: 1; 
+
+<main style="    flex: 1; 
     display: flex;
     justify-content: center; 
     align-items: center;     
-    text-align: center;
-}
-
-.texto-fechado {
-    font-family: 'Inter', sans-serif;
+    text-align: center;">
+    <div class="texto-fechado" 
+         wire:key="lw-2595669341-0" 
+         wire:id="w4SrrTiYvOeN20uYByyB"
+         style="font-family: 'Inter', sans-serif;
     font-size: 2rem;
     font-weight: bold;
     color: #797979;
-    text-transform: uppercase;
-}
-
-</style>
-<main>
-    <div class="texto-fechado" 
-         wire:key="lw-2595669341-0" 
-         wire:id="w4SrrTiYvOeN20uYByyB">
+    text-transform: uppercase;">
         Inscrições Fechadas
     </div>
 </main>
