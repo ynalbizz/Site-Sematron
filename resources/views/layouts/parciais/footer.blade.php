995b85c1b5c2e6efc4ec6b4d4b12ffe34b011d45
ynalbizz <cdavic13@gmail.com>
2026-04-09 12:07:44 -0300

    quase_final_pt2

--- resources/views/layouts/parciais/footer.blade.php
+++ resources/views/layouts/parciais/footer.blade.php
@@ -6,13 +6,11 @@
     <h1 class="Creditos-Rodape">Créditos</h1>
     <h1 class="Copyright-Rodape">Copyright © 2014–2025 Grupo SEMATRON. Todos os direitos reservados.</h1>
     <h1 class="Av-Trabalhador">Av. Trabalhador São-Carlense, 400 • EESC/USP • São Carlos — SP</h1>
-    <h1 class="Telefone">Tel: +55 (61) 98172-5281 • Email: sematron@eesc.usp.br</h1>
+    <h1 class="Telefone">Tel: +55 (61) 99838-3855 • Email: sematron@eesc.usp.br</h1>
 
     <div class="Redes-Sociais-Rodape">
         <a class="Borda-Rodape" href="https://www.instagram.com/sematronusp/">Instagram</a>
         <a class="Borda-Rodape" href="https://www.youtube.com/@sematronusp">YouTube</a>
         <a class="Borda-Rodape" href="/inicio">Site</a>
-        <!--Por favor não apague esse EasterEgg foi feito com muito carinho por mim ;)-->
-        <a class="Borda-Rodape-Gigante" href="https://www.ubirata.pr.gov.br/">Ubiratã</a>
     </div>
 </footer>
